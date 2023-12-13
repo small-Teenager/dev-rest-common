b3d07785ddb0dde0dc6d77fe7059471a4f03b3cc
dwzhangyaodong <zhangyaodong@gdhurray.com>
2023-12-13 09:59:05 +0800

    生成源码jar包

--- dev-rest-common/src/main/java/com/dev/rest/common/utils/IdWorker.java
+++ dev-rest-common/src/main/java/com/dev/rest/common/utils/IdWorker.java
@@ -28,25 +28,25 @@ public class IdWorker {
     private static final Logger log = LoggerFactory.getLogger(IdWorker.class);
 
     // 时间起始标记点，作为基准，一般取系统的最近时间（一旦确定不能变动）
-    private final static long twepoch = 1288834974657L;
+    private static final  long twepoch = 1288834974657L;
     // 机器标识位数
-    private final static long workerIdBits = 5L;
+    private static final  long workerIdBits = 5L;
     // 数据中心标识位数
-    private final static long datacenterIdBits = 5L;
+    private static final  long datacenterIdBits = 5L;
     // 机器ID最大值
-    private final static long maxWorkerId = -1L ^ (-1L << workerIdBits);
+    private static final  long maxWorkerId = -1L ^ (-1L << workerIdBits);
     // 数据中心ID最大值
-    private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+    private static final  long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
     // 毫秒内自增位
-    private final static long sequenceBits = 12L;
+    private static final  long sequenceBits = 12L;
     // 机器ID偏左移12位
-    private final static long workerIdShift = sequenceBits;
+    private static final  long workerIdShift = sequenceBits;
     // 数据中心ID左移17位
-    private final static long datacenterIdShift = sequenceBits + workerIdBits;
+    private static final  long datacenterIdShift = sequenceBits + workerIdBits;
     // 时间毫秒左移22位
-    private final static long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+    private static final  long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
 
-    private final static long sequenceMask = -1L ^ (-1L << sequenceBits);
+    private static final  long sequenceMask = -1L ^ (-1L << sequenceBits);
     /* 上次生产id时间戳 */
     private static long lastTimestamp = -1L;
     // 0，并发控制
@@ -99,11 +99,9 @@ public class IdWorker {
         }
         lastTimestamp = timestamp;
         // ID偏移组合生成最终的ID，并返回ID
-        long nextId = ((timestamp - twepoch) << timestampLeftShift)
+        return ((timestamp - twepoch) << timestampLeftShift)
                 | (datacenterId << datacenterIdShift)
                 | (workerId << workerIdShift) | sequence;
-
-        return nextId;
     }
 
     private long tilNextMillis(final long lastTimestamp) {
